5cae26c30e5a8aecb6851b9619248a4c9bdcf7b2
aSimonSheng <www.aSimonSheng@outlook.com>
2018-07-06 20:58:02 +0800

    重建了功能,并将代码跑起来了

--- front/js/register.js
+++ front/js/register.js
@@ -20,16 +20,16 @@ var vm = new Vue({
 		error_phone_message: '您输入的手机号格式不正确',
 	},
 	methods: {
-		check_username: function (){
-			var len = this.username.length;
-			if(len<5||len>20) {
-				this.error_name = true;
-			} else {
-				this.error_name = false;
-			}
-		//	ajax --> 发送
-			// 检查重名
-            if (this.error_name == false) {
+		   check_username: function (){
+            var len = this.username.length;
+            if(len<5||len>20) {
+                this.error_name = true;
+            } else {
+                this.error_name_message = '请输入5-20个字符的用户名';
+                this.error_name = false;
+            }
+            // 检查重名
+            if (this.error_name == false){
                 axios.get('http://127.0.0.1:8000'+'/users/usernames/' + this.username + '/count/', {
                         responseType: 'json'
                     })
@@ -45,31 +45,16 @@ var vm = new Vue({
                         console.log(error.response.data);
                     })
             }
-
-		},
-		check_pwd: function (){
-			var len = this.password.length;
-			if(len<8||len>20){
-				this.error_password = true;
-			} else {
-				this.error_password = false;
-			}		
-		},
-		check_cpwd: function (){
-			if(this.password!=this.password2) {
-				this.error_check_password = true;
-			} else {
-				this.error_check_password = false;
-			}		
-		},
-		check_phone: function (){
-			var re = /^1[345789]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_phone = false;
-			} else {
-				this.error_phone = true;
-			}
-			 if (this.error_phone == false) {
+        },
+		   check_phone: function (){
+            var re = /^1[345789]\d{9}$/;
+            if(re.test(this.mobile)) {
+                this.error_phone = false;
+            } else {
+                this.error_phone_message = '您输入的手机号格式不正确';
+                this.error_phone = true;
+            }
+            if (this.error_phone == false) {
                 axios.get('http://127.0.0.1:8000'+'/users/phones/'+ this.mobile + '/count/', {
                         responseType: 'json'
                     })
@@ -85,7 +70,7 @@ var vm = new Vue({
                         console.log(error.response.data);
                     })
             }
-		},
+        },
 		check_image_code: function (){
 			if(!this.image_code) {
 				this.error_image_code = true;
--- mall/mall/urls.py
+++ mall/mall/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # /users
     url(r'^users/', include('users.urls')),
 ]
